ba6e0b82c8a02c051ae1e402aedc6d337f0d23c8
Abidine Vall <67071565+Abidine-Vall@users.noreply.github.com>
2021-06-29 22:26:43 +0100

    Update Array_Sum.py

--- Array_Sum/Array_Sum.py
+++ Array_Sum/Array_Sum.py
@@ -1,3 +1,7 @@
+#Here We define a Function that will return an array with user specified inputs 
+#Note that the range in this example is [0,1000] for either the size of the array or it's elements 
+#The boolean variables is handy where its the paired with the constraint 
+
 def ar():
     My_List= list()
     input_valid= False
@@ -16,6 +20,7 @@ def ar():
                 break
             else:print("Array elements invalid!")
     return(My_List)
+#list and sum variables initialization before defining a function that will return the sum of the input array elements
 l=list()
 Sum=0
 def simpleArraySum(l):
@@ -24,7 +29,9 @@ def simpleArraySum(l):
          Sum += l[i]
                 
      return(Sum)
-if__name__=='__main__'
+#main function where we instanciated an array and created a new object with the simplearraysum method and then we displayed it !
+if __name__=='__main__':
  New_List=ar()
  List_Sum= simpleArraySum(New_List)
- print('The array Sum is: ',List_Sum)
+ print('The Sum is : ',List_Sum)
+#Thanks and hope this helped you!
